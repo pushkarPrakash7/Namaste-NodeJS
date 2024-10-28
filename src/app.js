c79fb63b6fcecde66013688b2acc59a3e3f2d46e
ashp79 <112823223+ashp79@users.noreply.github.com>
2024-10-28 13:35:15 +0530

    parameters in routing

--- src/app.js
+++ src/app.js
@@ -2,21 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user",(req,res)=>{
+
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
     res.send({firstName: "John", lastName: "Doe"});
 })
-
-app.post("/user",(req,res)=>{
-    res.send("data successfully sent to the database!");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("User deleted successfully!");
-});
-
-app.use("/test",(req,res)=>{
-    res.send("Hello world from the testing in browser!");
-});
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
