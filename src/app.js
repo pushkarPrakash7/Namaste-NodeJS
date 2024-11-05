eccb959f20042af3698249ae26c7df4a5cd2ccc7
ashp79 <112823223+ashp79@users.noreply.github.com>
2024-11-05 10:38:08 +0530

    API Level data sanitization

--- src/app.js
+++ src/app.js
@@ -4,77 +4,86 @@ const User = require("./models/user.js");
 const app = express();
 
 app.use(express.json());
-app.post('/signup', async (req, res)=>{
-    try{
+app.post('/signup', async (req, res) => {
+    
+    try {
         const user = new User(req.body);
         await user.save();
         res.status(201).send('User added successfully');
     }
-    catch(error){
+    catch (error) {
         res.status(400).send(error.message);
     }
 });
 
 
-app.get("/user",async (req, res) => {
+app.get("/user", async (req, res) => {
     const email = req.body.emailId;
-    try{
-        const user = await User.find({emailId: email});
-        if(user.length === 0){
+    try {
+        const user = await User.find({ emailId: email });
+        if (user.length === 0) {
             res.status(404).send("User not found")
         }
-        else 
+        else
             res.send(user);
     }
-    catch(error){
+    catch (error) {
         res.status(404).send(error.message);
     }
 });
 
-app.get("/feed",async (req, res)=>{
-    try { 
+app.get("/feed", async (req, res) => {
+    try {
         const user = await User.find({});
-        if(user.length === 0){
+        if (user.length === 0) {
             res.status(404).send("No users available in the database");
         }
-        else{
+        else {
             res.send(user);
         }
     }
-    catch(error){
+    catch (error) {
         res.status(500).send(error.message);
     }
 });
 
 //Deleting a particular user by id
-app.delete("/user",async(req,res)=>{
+app.delete("/user", async (req, res) => {
     const id = req.body.userId;
-    try{
+    try {
         const user = await User.findByIdAndDelete(id);
-        if(!user){
+        if (!user) {
             res.status(404).send("User not found")
         }
         else
             res.send("User deleted successfully");
     }
-    catch(error){
+    catch (error) {
         res.status(500).send(error.message);
     }
 });
 
 //Updating the records of a particular user
-app.patch("/user" , async(req,res)=>{
+app.patch("/user", async (req, res) => {
     const id = req.body.userId;
     const data = req.body;
-    try{
-        const user = await User.findByIdAndUpdate({_id: id} , data);
+    const ALLOWED_UPDTAES = ["photoURL", "about", "gender", "age"]
+
+    const isAllowedUpdate = Object.keys(data).every((k) =>
+        ALLOWED_UPDTAES.includes(k));
+
+    if(!isAllowedUpdate){
+        res.status(400).send("Invalid updates, only photoURL, about, gender, and age are allowed");
+    }
+    try {
+        const user = await User.findByIdAndUpdate({ _id: id }, data);
         res.send("USER UPDATED SUCCESSFULLY");
     }
-    catch(error){
+    catch (error) {
         res.status(404).send(error.message);
     }
 });
-connectDB().then(()=>{
+connectDB().then(() => {
     console.log("Database connection established!");
     app.listen(3000, () => {
         console.log('Server is running on port 3000');
--- src/models/user.js
+++ src/models/user.js
@@ -2,30 +2,53 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     firstName:{
         type: String,
-        required: true
+        required: true,
+        minLength: 2,
+        maxLength: 15,
     },
     lastName:{
         type: String,
-        required: true
+        required: true,
+        minLength: 2,
+        maxLength: 15,
     },
     emailId :{
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        trim: true,
+        maxLength: 30,
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minLength: 8,
+        maxLength: 20,
     },
     age : {
         type: Number,
-        required: true
+        required: true,
+        min: 18,
+        max: 99
     },
     gender : {
         type: String,
-        required: true
+        required: true,
+        enum: ['Male', 'Female', 'Other']
+    },
+    photoURL : {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOtu74pEiq7ofeQeTsco0migV16zZoBwSlGg&s"
+    },
+    about: {
+        type: String,
+        default: "This describes the User"
+    },
+    skills: {
+        type: [String],
     }
-});
+},{timestamps : true});
 
 const userModel = mongoose.model('User', userSchema);
 module.exports = userModel;
